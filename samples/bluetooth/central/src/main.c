da4f1b761ceced49048960200601f0929156fd58
Rico, Jonathan <jonathan.rico@nordicsemi.no>
2020-09-01 15:40:38 +0200

    Trigger sdu_len RX bug

--- samples/bluetooth/central/src/main.c
+++ samples/bluetooth/central/src/main.c
@@ -17,7 +17,192 @@
 #include <bluetooth/conn.h>
 #include <bluetooth/uuid.h>
 #include <bluetooth/gatt.h>
+#include <bluetooth/l2cap.h>
 #include <sys/byteorder.h>
+
+/* Two different ways to trigger the bug */
+#define TRIGGER_SDU_BUG 0
+#define TRIGGER_SDU_BUG_AGAIN 1
+
+#define HOST_L2CAP_NETBUF_COUNT 5
+#define HOST_MAX_L2CAP_CHANNELS 4
+
+#define MAX_L2CAP_DATA_LEN 128
+
+#if TRIGGER_SDU_BUG
+#define L2CAP_SPAM_DATA_LEN 80
+#elif TRIGGER_SDU_BUG_AGAIN
+#define L2CAP_SPAM_DATA_LEN 27
+#else
+#define L2CAP_SPAM_DATA_LEN 50
+#endif
+
+#define L2CAP_RX_MTU (L2CAP_SPAM_DATA_LEN + 4)
+
+static uint8_t l2cap_data[L2CAP_SPAM_DATA_LEN] = {0xD3};
+
+bool periph_connected = 0;
+bool l2cap_chan_connected = 0;
+uint32_t l2cap_handle = 0;
+
+#if TRIGGER_SDU_BUG_AGAIN
+NET_BUF_POOL_DEFINE(l2cap_pool, HOST_L2CAP_NETBUF_COUNT,
+		    BT_L2CAP_BUF_SIZE(L2CAP_SPAM_DATA_LEN), 0, NULL);
+#else
+NET_BUF_POOL_DEFINE(l2cap_pool, HOST_L2CAP_NETBUF_COUNT,
+		    BT_L2CAP_BUF_SIZE(MAX_L2CAP_DATA_LEN), 0, NULL);
+#endif
+
+static struct bt_l2cap_le_chan l2cap_channels[HOST_MAX_L2CAP_CHANNELS];
+
+#if !(TRIGGER_SDU_BUG || TRIGGER_SDU_BUG_AGAIN)
+static struct net_buf *l2cap_chan_alloc_buf_cb(struct bt_l2cap_chan *chan);
+#endif
+static void l2cap_chan_connected_cb(struct bt_l2cap_chan *chan);
+static void l2cap_chan_status_cb(struct bt_l2cap_chan *chan, atomic_t *status);
+static int l2cap_chan_recv_cb(struct bt_l2cap_chan *chan, struct net_buf *buf);
+static void l2cap_chan_sent_cb(struct bt_l2cap_chan *chan);
+
+static struct bt_l2cap_chan_ops l2cap_chan_ops = {
+#if !(TRIGGER_SDU_BUG || TRIGGER_SDU_BUG_AGAIN)
+	.alloc_buf = l2cap_chan_alloc_buf_cb,
+#endif
+	.connected = l2cap_chan_connected_cb,
+	.status = l2cap_chan_status_cb,
+	.recv = l2cap_chan_recv_cb,
+	.sent = l2cap_chan_sent_cb
+};
+
+static int l2cap_chan_recv_cb(struct bt_l2cap_chan *chan, struct net_buf *buf)
+{
+	printk("%s\n", __func__);
+	return 0;
+}
+
+static void l2cap_chan_sent_cb(struct bt_l2cap_chan *chan)
+{
+	printk("%s\n", __func__);
+}
+
+#if (TRIGGER_SDU_BUG || TRIGGER_SDU_BUG_AGAIN)
+static struct net_buf *l2cap_chan_alloc_buf_cb(struct bt_l2cap_chan *chan)
+{
+	return net_buf_alloc(&l2cap_pool, K_NO_WAIT);
+}
+#endif
+
+static void l2cap_chan_status_cb(struct bt_l2cap_chan *chan, atomic_t *status)
+{
+	return;
+}
+
+static void l2cap_chan_connected_cb(struct bt_l2cap_chan *l2cap_chan)
+{
+	l2cap_chan_connected = 1;
+}
+
+static struct bt_l2cap_le_chan *get_free_l2cap_le_chan(void)
+{
+	for (int i = 0; i < HOST_MAX_L2CAP_CHANNELS; i++) {
+		struct bt_l2cap_le_chan *le_chan = &l2cap_channels[i];
+		printk("%s enter loop\n", __func__);
+
+		if (le_chan->chan.status != BT_L2CAP_DISCONNECTED) {
+			printk("%s chan status: %ls\n", __func__, le_chan->chan.status );
+			continue;
+		}
+
+		printk("%s memset\n", __func__);
+		memset(le_chan, 0, sizeof(le_chan));
+		return le_chan;
+	}
+
+	printk("%s return null\n", __func__);
+	return NULL;
+}
+
+static int l2cap_server_accept_cb(struct bt_conn *conn,
+				  struct bt_l2cap_chan **chan)
+{
+	struct bt_l2cap_le_chan *le_chan = get_free_l2cap_le_chan();
+
+	if (le_chan == NULL) {
+		return -ENOMEM;
+	}
+
+	memset(le_chan, 0, sizeof(*le_chan));
+	le_chan->chan.ops = &l2cap_chan_ops;
+	le_chan->rx.mtu = MAX_L2CAP_DATA_LEN;
+	*chan = &le_chan->chan;
+
+	return 0;
+}
+
+static struct bt_l2cap_server l2cap_server = { .accept = l2cap_server_accept_cb };
+
+int test_bt_l2cap_server_register(uint16_t psm, bt_security_t sec_level)
+{
+	l2cap_server.psm = psm;
+	l2cap_server.sec_level = sec_level;
+
+	return bt_l2cap_server_register(&l2cap_server);
+}
+
+uint16_t test_bt_l2cap_server_get_psm(void)
+{
+	return l2cap_server.psm;
+}
+
+int test_bt_l2cap_chan_connect(struct bt_conn* conn, uint16_t psm,
+			       uint16_t rx_mtu)
+{
+	l2cap_handle = 0;
+
+	/* struct bt_l2cap_le_chan *le_chan = get_free_l2cap_le_chan(); */
+	struct bt_l2cap_le_chan *le_chan = &l2cap_channels[0];
+
+	if (le_chan == NULL) {
+		return -ENOMEM;
+	}
+
+	le_chan->chan.ops = &l2cap_chan_ops;
+	le_chan->rx.mtu = rx_mtu;
+
+	int ret = bt_l2cap_chan_connect(conn, &le_chan->chan, psm);
+
+	if (ret == 0) {
+		l2cap_handle = (uint32_t)&le_chan->chan;
+	}
+
+	return ret;
+}
+
+int test_bt_l2cap_chan_disconnect(uint32_t l2cap_handle)
+{
+	struct bt_l2cap_chan *chan = (struct bt_l2cap_chan *)l2cap_handle;
+
+	return bt_l2cap_chan_disconnect(chan);
+}
+
+int test_bt_l2cap_chan_send(uint32_t l2cap_handle)
+{
+	struct bt_l2cap_chan *chan = (struct bt_l2cap_chan *)l2cap_handle;
+
+	struct net_buf *buf = net_buf_alloc(&l2cap_pool, K_NO_WAIT);
+	if (buf == NULL) {
+		return -ENOMEM;
+	}
+
+	net_buf_add_mem(buf, l2cap_data, L2CAP_SPAM_DATA_LEN);
+
+	int ret = bt_l2cap_chan_send(chan, buf);
+
+	if (ret < 0) {
+		net_buf_unref(buf);
+	}
+
+	return ret;
+}
 
 static void start_scan(void);
 
@@ -40,10 +225,10 @@ static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 	}
 
 	bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
-	printk("Device found: %s (RSSI %d)\n", addr_str, rssi);
+	/* printk("Device found: %s (RSSI %d)\n", addr_str, rssi); */
 
 	/* connect only to devices in close proximity */
-	if (rssi < -70) {
+	if (rssi < -50) {
 		return;
 	}
 
@@ -95,7 +280,7 @@ static void connected(struct bt_conn *conn, uint8_t err)
 
 	printk("Connected: %s\n", addr);
 
-	bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+	periph_connected = 1;
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
@@ -125,6 +310,10 @@ void main(void)
 {
 	int err;
 
+	/* Clear channel array */
+	for(int i=0; i<HOST_MAX_L2CAP_CHANNELS; i++)
+		memset(&l2cap_channels[i], 0, sizeof(l2cap_channels[i]));
+
 	err = bt_enable(NULL);
 	if (err) {
 		printk("Bluetooth init failed (err %d)\n", err);
@@ -136,4 +325,40 @@ void main(void)
 	bt_conn_cb_register(&conn_callbacks);
 
 	start_scan();
+
+	printk("Start wait...\n");
+	while(!periph_connected)
+	{
+		k_msleep(1000);
+		printk("Waiting for connection...\n");
+	}
+
+	printk("Starting L2CAP test\n");
+	k_msleep(2000);
+
+	printk("Connecting L2CAP channel\n");
+	int error = test_bt_l2cap_chan_connect(default_conn, 0x80, L2CAP_RX_MTU);
+	if(error != 0)
+		printk("chan_connect error %d\n", error);
+
+	while(!l2cap_chan_connected)
+	{
+		k_msleep(1000);
+		printk("Waiting for l2cap channel connection...\n");
+	}
+
+	printk("Sending payload\n");
+	error = test_bt_l2cap_chan_send(l2cap_handle);
+	if(error != 0)
+	{
+		printk("chan_send error %d\n", error);
+	}
+
+	while(1)
+	{
+		k_msleep(1000);
+	}
+
+	printk("Disconnecting\n");
+	bt_conn_disconnect(default_conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
 }
--- samples/bluetooth/peripheral_hr/src/main.c
+++ samples/bluetooth/peripheral_hr/src/main.c
@@ -19,8 +19,121 @@
 #include <bluetooth/conn.h>
 #include <bluetooth/uuid.h>
 #include <bluetooth/gatt.h>
-#include <bluetooth/services/bas.h>
-#include <bluetooth/services/hrs.h>
+#include <bluetooth/l2cap.h>
+
+#define TRIGGER_SDU_BUG 1
+
+#define HOST_L2CAP_NETBUF_COUNT 5
+#define HOST_MAX_L2CAP_CHANNELS 4
+
+#define MAX_L2CAP_DATA_LEN 128
+
+bool periph_connected = 0;
+bool l2cap_chan_connected = 0;
+uint32_t l2cap_handle = 0;
+
+NET_BUF_POOL_DEFINE(l2cap_pool, HOST_L2CAP_NETBUF_COUNT,
+		    BT_L2CAP_BUF_SIZE(MAX_L2CAP_DATA_LEN), 0, NULL);
+
+static struct bt_l2cap_le_chan l2cap_channels[HOST_MAX_L2CAP_CHANNELS];
+
+
+#if !(TRIGGER_SDU_BUG)
+static struct net_buf *l2cap_chan_alloc_buf_cb(struct bt_l2cap_chan *chan);
+#endif
+static void l2cap_chan_connected_cb(struct bt_l2cap_chan *chan);
+static void l2cap_chan_status_cb(struct bt_l2cap_chan *chan, atomic_t *status);
+static int l2cap_chan_recv_cb(struct bt_l2cap_chan *chan, struct net_buf *buf);
+static void l2cap_chan_sent_cb(struct bt_l2cap_chan *chan);
+
+static struct bt_l2cap_chan_ops l2cap_chan_ops = {
+#if !(TRIGGER_SDU_BUG)
+	.alloc_buf = l2cap_chan_alloc_buf_cb,
+#endif
+	.connected = l2cap_chan_connected_cb,
+	.status = l2cap_chan_status_cb,
+	.recv = l2cap_chan_recv_cb,
+	.sent = l2cap_chan_sent_cb
+};
+
+static int l2cap_chan_recv_cb(struct bt_l2cap_chan *chan, struct net_buf *buf)
+{
+	printk("%s\n", __func__);
+	return 0;
+}
+
+static void l2cap_chan_sent_cb(struct bt_l2cap_chan *chan)
+{
+	printk("%s\n", __func__);
+}
+
+#if !(TRIGGER_SDU_BUG)
+static struct net_buf *l2cap_chan_alloc_buf_cb(struct bt_l2cap_chan *chan)
+{
+	printk("%s\n", __func__);
+	return net_buf_alloc(&l2cap_pool, K_NO_WAIT);
+}
+#endif
+
+static void l2cap_chan_status_cb(struct bt_l2cap_chan *chan, atomic_t *status)
+{
+	printk("%s\n", __func__);
+	return;
+}
+
+static void l2cap_chan_connected_cb(struct bt_l2cap_chan *l2cap_chan)
+{
+	printk("%s\n", __func__);
+	l2cap_chan_connected = 1;
+}
+
+static struct bt_l2cap_le_chan *get_free_l2cap_le_chan(void)
+{
+	printk("%s\n", __func__);
+	for (int i = 0; i < HOST_MAX_L2CAP_CHANNELS; i++) {
+		struct bt_l2cap_le_chan *le_chan = &l2cap_channels[i];
+
+		if (le_chan->chan.status != BT_L2CAP_DISCONNECTED) {
+			continue;
+		}
+
+		memset(le_chan, 0, sizeof(le_chan));
+		return le_chan;
+	}
+
+	return NULL;
+}
+
+static int l2cap_server_accept_cb(struct bt_conn *conn,
+				  struct bt_l2cap_chan **chan)
+{
+	printk("%s\n", __func__);
+
+	/* struct bt_l2cap_le_chan *le_chan = get_free_l2cap_le_chan(); */
+	struct bt_l2cap_le_chan *le_chan = &l2cap_channels[0];
+
+	if (le_chan == NULL) {
+		return -ENOMEM;
+	}
+
+	memset(le_chan, 0, sizeof(*le_chan));
+	le_chan->chan.ops = &l2cap_chan_ops;
+	le_chan->rx.mtu = MAX_L2CAP_DATA_LEN;
+	*chan = &le_chan->chan;
+
+	return 0;
+}
+
+static struct bt_l2cap_server l2cap_server = { .accept = l2cap_server_accept_cb };
+
+int test_bt_l2cap_server_register(uint16_t psm, bt_security_t sec_level)
+{
+	printk("%s\n", __func__);
+	l2cap_server.psm = psm;
+	l2cap_server.sec_level = sec_level;
+
+	return bt_l2cap_server_register(&l2cap_server);
+}
 
 struct bt_conn *default_conn;
 
@@ -82,35 +195,10 @@ static struct bt_conn_auth_cb auth_cb_display = {
 	.cancel = auth_cancel,
 };
 
-static void bas_notify(void)
-{
-	uint8_t battery_level = bt_gatt_bas_get_battery_level();
-
-	battery_level--;
-
-	if (!battery_level) {
-		battery_level = 100U;
-	}
-
-	bt_gatt_bas_set_battery_level(battery_level);
-}
-
-static void hrs_notify(void)
-{
-	static uint8_t heartrate = 90U;
-
-	/* Heartrate measurements simulation */
-	heartrate++;
-	if (heartrate == 160U) {
-		heartrate = 90U;
-	}
-
-	bt_gatt_hrs_notify(heartrate);
-}
-
 void main(void)
 {
 	int err;
+	(void)l2cap_server;
 
 	err = bt_enable(NULL);
 	if (err) {
@@ -120,19 +208,15 @@ void main(void)
 
 	bt_ready();
 
+	printk("Registering server\n");
+	test_bt_l2cap_server_register(0, BT_SECURITY_L1);
+	printk("Registering callbacks\n");
 	bt_conn_cb_register(&conn_callbacks);
 	bt_conn_auth_cb_register(&auth_cb_display);
+	printk("Main loop\n");
 
-	/* Implement notification. At the moment there is no suitable way
-	 * of starting delayed work so we do it here
-	 */
 	while (1) {
 		k_sleep(K_SECONDS(1));
 
-		/* Heartrate measurements simulation */
-		hrs_notify();
-
-		/* Battery level simulation */
-		bas_notify();
 	}
 }
