4554137d55f2d66bd504c5d95e11a76b14e0b8f1
Jonathan Rico <jonathan.rico@nordicsemi.no>
2021-09-03 08:23:16 +0200

    dnm: modify peripheral_hr sample with custom gatt char
    To be used with a customized central_hr that will spam-write to it.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- samples/bluetooth/peripheral_hr/src/main.c
+++ samples/bluetooth/peripheral_hr/src/main.c
@@ -13,22 +13,75 @@
 #include <sys/printk.h>
 #include <sys/byteorder.h>
 #include <zephyr.h>
+#include <logging/log.h>
 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/conn.h>
 #include <bluetooth/uuid.h>
 #include <bluetooth/gatt.h>
-#include <bluetooth/services/bas.h>
-#include <bluetooth/services/hrs.h>
+
+LOG_MODULE_REGISTER(main);
+
+#define TEST_SERVICE_UUID        BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef0)
+#define TEST_CHARACTERISTIC_UUID BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef2)
+#define TEST_DESCRIPTOR_UUID     BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef4)
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-	BT_DATA_BYTES(BT_DATA_UUID16_ALL,
-		      BT_UUID_16_ENCODE(BT_UUID_HRS_VAL),
-		      BT_UUID_16_ENCODE(BT_UUID_BAS_VAL),
-		      BT_UUID_16_ENCODE(BT_UUID_DIS_VAL))
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL,
+		      TEST_SERVICE_UUID)
 };
+
+#define DAT_LEN 512
+char test_gatt_data[DAT_LEN] = {0x01};
+
+static ssize_t read_current_time(struct bt_conn *conn,
+				 const struct bt_gatt_attr *attr, void *buf,
+				 uint16_t len, uint16_t offset)
+{
+	LOG_INF("Read attribute");
+	return bt_gatt_attr_read(conn, attr, buf, len, offset,
+				 test_gatt_data,
+				 sizeof(test_gatt_data));
+}
+
+static ssize_t write_current_time(struct bt_conn *conn,
+				  const struct bt_gatt_attr *attr,
+				  const void *buf, uint16_t len,
+				  uint16_t offset, uint8_t flags)
+{
+	LOG_INF("Write attribute");
+
+	if(len > DAT_LEN) {
+		LOG_INF("Len too high, truncating");
+		len = DAT_LEN;
+	}
+	memcpy(test_gatt_data, buf, len);
+
+	LOG_HEXDUMP_INF(test_gatt_data, len, "Processed data: ");
+
+	return len;
+}
+
+void on_gatt_ccc_changed_cb(const struct bt_gatt_attr *attr, uint16_t value)
+{
+	ARG_UNUSED(attr);
+	ARG_UNUSED(value);
+	printk("CCC changed\n");
+}
+
+BT_GATT_SERVICE_DEFINE(
+	cts,
+	BT_GATT_PRIMARY_SERVICE(BT_UUID_DECLARE_128(TEST_SERVICE_UUID)),
+	BT_GATT_CHARACTERISTIC(BT_UUID_DECLARE_128(TEST_CHARACTERISTIC_UUID),
+			       BT_GATT_CHRC_READ | BT_GATT_CHRC_WRITE | BT_GATT_CHRC_WRITE_WITHOUT_RESP,
+			       BT_GATT_PERM_READ | BT_GATT_PERM_WRITE,
+			       read_current_time, write_current_time,
+			       (void*)test_gatt_data),
+	BT_GATT_DESCRIPTOR(BT_UUID_DECLARE_128(TEST_DESCRIPTOR_UUID), BT_GATT_PERM_READ,
+			   NULL, NULL, NULL)
+);
 
 static void connected(struct bt_conn *conn, uint8_t err)
 {
@@ -77,32 +130,6 @@ static struct bt_conn_auth_cb auth_cb_display = {
 	.cancel = auth_cancel,
 };
 
-static void bas_notify(void)
-{
-	uint8_t battery_level = bt_bas_get_battery_level();
-
-	battery_level--;
-
-	if (!battery_level) {
-		battery_level = 100U;
-	}
-
-	bt_bas_set_battery_level(battery_level);
-}
-
-static void hrs_notify(void)
-{
-	static uint8_t heartrate = 90U;
-
-	/* Heartrate measurements simulation */
-	heartrate++;
-	if (heartrate == 160U) {
-		heartrate = 90U;
-	}
-
-	bt_hrs_notify(heartrate);
-}
-
 void main(void)
 {
 	int err;
@@ -114,6 +141,7 @@ void main(void)
 	}
 
 	bt_ready();
+	memcpy(test_gatt_data, "Hello world !", sizeof("Hello world !"));
 
 	bt_conn_cb_register(&conn_callbacks);
 	bt_conn_auth_cb_register(&auth_cb_display);
@@ -124,10 +152,5 @@ void main(void)
 	while (1) {
 		k_sleep(K_SECONDS(1));
 
-		/* Heartrate measurements simulation */
-		hrs_notify();
-
-		/* Battery level simulation */
-		bas_notify();
 	}
 }
