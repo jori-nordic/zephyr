0283557517b441bf3af802ff87aac5e0f5aa67b6
Jonathan Rico <jonathan.rico@nordicsemi.no>
2022-04-13 15:17:54 +0200

    [WIP] drivers: uart: read multiple bytes in single DMA transfer
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- drivers/serial/uart_nrfx_uarte.c
+++ drivers/serial/uart_nrfx_uarte.c
@@ -114,6 +114,8 @@ struct uarte_async_cb {
 struct uarte_nrfx_int_driven {
 	uart_irq_callback_user_data_t cb; /**< Callback function pointer */
 	void *cb_data; /**< Callback function arg */
+	uint8_t *rx_buffer;
+	uint16_t rx_buff_size;
 	uint8_t *tx_buffer;
 	uint16_t tx_buff_size;
 	volatile bool disable_tx_irq;
@@ -1510,7 +1512,11 @@ static int uarte_nrfx_poll_in(const struct device *dev, unsigned char *c)
 		return -1;
 	}
 
+#ifdef UARTE_INTERRUPT_DRIVEN
+	*c = *data->int_driven->rx_buffer;
+#else
 	*c = data->rx_data;
+#endif
 
 	/* clear the interrupt */
 	nrf_uarte_event_clear(uarte, NRF_UARTE_EVENT_ENDRX);
@@ -1604,9 +1610,16 @@ static int uarte_nrfx_fifo_read(const struct device *dev,
 		/* Clear the interrupt */
 		nrf_uarte_event_clear(uarte, NRF_UARTE_EVENT_ENDRX);
 
-		/* Receive a character */
-		rx_data[num_rx++] = (uint8_t)data->rx_data;
+		uint32_t rx_amount = nrf_uarte_rx_amount_get(uarte);
+		num_rx = MIN(size, rx_amount);
 
+		memcpy(rx_data, data->int_driven->rx_buffer, num_rx);
+
+		/* Attempt reading the rest in a single transfer */
+		if(num_rx != size)
+			nrf_uarte_rx_buffer_set(uarte, data->int_driven->rx_buffer, size - num_rx);
+		else
+			nrf_uarte_rx_buffer_set(uarte, data->int_driven->rx_buffer, 1);
 		nrf_uarte_task_trigger(uarte, NRF_UARTE_TASK_STARTRX);
 	}
 
@@ -1831,7 +1844,11 @@ static int uarte_instance_init(const struct device *dev,
 		if (!cfg->disable_rx) {
 			nrf_uarte_event_clear(uarte, NRF_UARTE_EVENT_ENDRX);
 
+#ifdef UARTE_INTERRUPT_DRIVEN
+			nrf_uarte_rx_buffer_set(uarte, data->int_driven->rx_buffer, 1);
+#else
 			nrf_uarte_rx_buffer_set(uarte, &data->rx_data, 1);
+#endif
 			nrf_uarte_task_trigger(uarte, NRF_UARTE_TASK_STARTRX);
 		}
 	}
@@ -2124,8 +2141,13 @@ static int uarte_nrfx_pm_action(const struct device *dev,
 		(static uint8_t uarte##idx##_tx_buffer[\
 			MIN(CONFIG_UART_##idx##_NRF_TX_BUFFER_SIZE,	       \
 			    BIT_MASK(UARTE##idx##_EASYDMA_MAXCNT_SIZE))];      \
+		 static uint8_t uarte##idx##_rx_buffer[			\
+			MIN(CONFIG_UART_##idx##_NRF_TX_BUFFER_SIZE,	       \
+			    BIT_MASK(UARTE##idx##_EASYDMA_MAXCNT_SIZE))];      \
 		 static struct uarte_nrfx_int_driven			       \
 			uarte##idx##_int_driven = {			       \
+				.rx_buffer = uarte##idx##_rx_buffer,	       \
+				.rx_buff_size = sizeof(uarte##idx##_rx_buffer),\
 				.tx_buffer = uarte##idx##_tx_buffer,	       \
 				.tx_buff_size = sizeof(uarte##idx##_tx_buffer),\
 			};))
