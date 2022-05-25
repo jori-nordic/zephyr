c382d0d98e83aca6ca84910ccde312bd75f47882
Jonathan Rico <jonathan.rico@nordicsemi.no>
2022-05-25 09:17:47 +0200

    Shell: add kernel sleep command
    Basic sleep command for scripting purposes.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- subsys/shell/modules/kernel_service.c
+++ subsys/shell/modules/kernel_service.c
@@ -15,6 +15,7 @@
 #include <zephyr/drivers/timer/system_timer.h>
 #include <zephyr/kernel.h>
 #include <kernel_internal.h>
+#include <stdlib.h>
 
 static int cmd_kernel_version(const struct shell *shell,
 			      size_t argc, char **argv)
@@ -218,6 +219,17 @@ static int cmd_kernel_stacks(const struct shell *shell,
 }
 #endif
 
+static int cmd_kernel_sleep(const struct shell *shell,
+			    size_t argc, char **argv)
+{
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	k_msleep(strtoul(argv[1], NULL, 10));
+
+	return 0;
+}
+
 #if defined(CONFIG_REBOOT)
 static int cmd_kernel_reboot_warm(const struct shell *shell,
 				  size_t argc, char **argv)
@@ -262,6 +274,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_kernel,
 #endif
 	SHELL_CMD(uptime, NULL, "Kernel uptime.", cmd_kernel_uptime),
 	SHELL_CMD(version, NULL, "Kernel version.", cmd_kernel_version),
+	SHELL_CMD_ARG(sleep, NULL, "ms", cmd_kernel_sleep, 2, 0),
 	SHELL_SUBCMD_SET_END /* Array terminated. */
 );
 
