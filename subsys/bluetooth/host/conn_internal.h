16d0b7686da8edcb3b40574ddc27224e1673f7b2
Herman Berget <herman.berget@nordicsemi.no>
2022-05-31 13:35:34 +0200

    Bluetooth: Host: Add err parameter to bt_conn_tx_cb_t
    This gives more information back to the supplier of the callback.
    
    Signed-off-by: Herman Berget <herman.berget@nordicsemi.no>

--- subsys/bluetooth/host/conn_internal.h
+++ subsys/bluetooth/host/conn_internal.h
@@ -134,7 +134,7 @@ struct bt_conn_iso {
 	enum bt_iso_chan_type type;
 };
 
-typedef void (*bt_conn_tx_cb_t)(struct bt_conn *conn, void *user_data);
+typedef void (*bt_conn_tx_cb_t)(struct bt_conn *conn, void *user_data, int err);
 
 struct bt_conn_tx {
 	sys_snode_t node;
