75db6b301f792f9f56d77bd6d484ea12de59a0ae
Jonathan Rico <jonathan.rico@nordicsemi.no>
2022-12-05 13:08:39 +0100

    workaround
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- drivers/bluetooth/hci/rpmsg.c
+++ drivers/bluetooth/hci/rpmsg.c
@@ -29,45 +29,9 @@ LOG_MODULE_REGISTER(bt_hci_driver);
 static struct ipc_ept hci_ept;
 static K_SEM_DEFINE(ipc_bound_sem, 0, 1);
 
-static bool is_hci_event_discardable(const uint8_t *evt_data)
-{
-	uint8_t evt_type = evt_data[0];
-
-	switch (evt_type) {
-#if defined(CONFIG_BT_BREDR)
-	case BT_HCI_EVT_INQUIRY_RESULT_WITH_RSSI:
-	case BT_HCI_EVT_EXTENDED_INQUIRY_RESULT:
-		return true;
-#endif
-	case BT_HCI_EVT_LE_META_EVENT: {
-		uint8_t subevt_type = evt_data[sizeof(struct bt_hci_evt_hdr)];
-
-		switch (subevt_type) {
-		case BT_HCI_EVT_LE_ADVERTISING_REPORT:
-			return true;
-#if defined(CONFIG_BT_EXT_ADV)
-		case BT_HCI_EVT_LE_EXT_ADVERTISING_REPORT:
-		{
-			const struct bt_hci_evt_le_ext_advertising_report *ext_adv =
-				(void *)&evt_data[3];
-
-			return (ext_adv->num_reports == 1) &&
-				   ((ext_adv->adv_info[0].evt_type &
-					 BT_HCI_LE_ADV_EVT_TYPE_LEGACY) != 0);
-		}
-#endif
-		default:
-			return false;
-		}
-	}
-	default:
-		return false;
-	}
-}
-
+struct net_buf *bt_buf_get_evt_2(const uint8_t *evt_data, k_timeout_t timeout);
 static struct net_buf *bt_rpmsg_evt_recv(const uint8_t *data, size_t remaining)
 {
-	bool discardable;
 	struct bt_hci_evt_hdr hdr;
 	struct net_buf *buf;
 	size_t buf_tailroom;
@@ -77,10 +41,7 @@ static struct net_buf *bt_rpmsg_evt_recv(const uint8_t *data, size_t remaining)
 		return NULL;
 	}
 
-	discardable = is_hci_event_discardable(data);
-
 	memcpy((void *)&hdr, data, sizeof(hdr));
-	data += sizeof(hdr);
 	remaining -= sizeof(hdr);
 
 	if (remaining != hdr.len) {
@@ -89,16 +50,11 @@ static struct net_buf *bt_rpmsg_evt_recv(const uint8_t *data, size_t remaining)
 	}
 	LOG_DBG("len %u", hdr.len);
 
-	do {
-		buf = bt_buf_get_evt(hdr.evt, discardable, discardable ? K_NO_WAIT : K_SECONDS(10));
-		if (!buf) {
-			if (discardable) {
-				LOG_DBG("Discardable buffer pool full, ignoring event");
-				return buf;
-			}
-			LOG_WRN("Couldn't allocate a buffer after waiting 10 seconds.");
-		}
-	} while (!buf);
+	buf = bt_buf_get_evt_2(data, K_SECONDS(30));
+	if (!buf) return NULL;
+	/* __ASSERT_NO_MSG(buf); */
+
+	data += sizeof(hdr);
 
 	net_buf_add_mem(buf, &hdr, sizeof(hdr));
 
