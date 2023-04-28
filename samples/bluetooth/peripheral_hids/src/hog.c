7db141ca97196ae06518574ed54341f6f71f51a4
Jonathan Rico <jonathan.rico@nordicsemi.no>
2023-04-28 17:23:34 +0200

    make it work on instagram
    scrolls using a mouse flick gesture
    doesn't work on reels (have to swipe right)
    doesn't work on image feed (too fast)
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- samples/bluetooth/peripheral_hids/src/hog.c
+++ samples/bluetooth/peripheral_hids/src/hog.c
@@ -59,36 +59,34 @@ static struct hids_report input = {
 static uint8_t simulate_input;
 static uint8_t ctrl_point;
 
-static const uint8_t report_map[] = {
-	0x05, 0x01,       /* Usage Page (Generic Desktop) */
-	0x09, 0x06,       /* Usage (Keyboard) */
-	0xA1, 0x01,       /* Collection (Application) */
-
-	/* Keys */
-	0x85, 0x01,
-	0x05, 0x07,       /* Usage Page (Key Codes) */
-	0x19, 0xe0,       /* Usage Minimum (224) */
-	0x29, 0xe7,       /* Usage Maximum (231) */
-	0x15, 0x00,       /* Logical Minimum (0) */
-	0x25, 0x01,       /* Logical Maximum (1) */
-	0x75, 0x01,       /* Report Size (1) */
-	0x95, 0x08,       /* Report Count (8) */
-	0x81, 0x02,       /* Input (Data, Variable, Absolute) */
-
-	0x95, 0x01,       /* Report Count (1) */
-	0x75, 0x08,       /* Report Size (8) */
-	0x81, 0x01,       /* Input (Constant) reserved byte(1) */
-
-	0x95, 0x06,       /* Report Count (6) */
-	0x75, 0x08,       /* Report Size (8) */
-	0x15, 0x00,       /* Logical Minimum (0) */
-	0x25, 0x65,       /* Logical Maximum (101) */
-	0x05, 0x07,       /* Usage Page (Key codes) */
-	0x19, 0x00,       /* Usage Minimum (0) */
-	0x29, 0x65,       /* Usage Maximum (101) */
-	0x81, 0x00,       /* Input (Data, Array) Key array(6 bytes) */
-
-	0xC0              /* End Collection (Application) */
+static uint8_t report_map[] = {
+	0x05, 0x01, /* Usage Page (Generic Desktop Ctrls) */
+	0x09, 0x02, /* Usage (Mouse) */
+	0xA1, 0x01, /* Collection (Application) */
+	0x85, 0x01, /*	 Report Id (1) */
+	0x09, 0x01, /*   Usage (Pointer) */
+	0xA1, 0x00, /*   Collection (Physical) */
+	0x05, 0x09, /*     Usage Page (Button) */
+	0x19, 0x01, /*     Usage Minimum (0x01) */
+	0x29, 0x03, /*     Usage Maximum (0x03) */
+	0x15, 0x00, /*     Logical Minimum (0) */
+	0x25, 0x01, /*     Logical Maximum (1) */
+	0x95, 0x03, /*     Report Count (3) */
+	0x75, 0x01, /*     Report Size (1) */
+	0x81, 0x02, /*     Input (Data,Var,Abs,No Wrap,Linear,...) */
+	0x95, 0x01, /*     Report Count (1) */
+	0x75, 0x05, /*     Report Size (5) */
+	0x81, 0x03, /*     Input (Const,Var,Abs,No Wrap,Linear,...) */
+	0x05, 0x01, /*     Usage Page (Generic Desktop Ctrls) */
+	0x09, 0x30, /*     Usage (X) */
+	0x09, 0x31, /*     Usage (Y) */
+	0x15, 0x81, /*     Logical Minimum (129) */
+	0x25, 0x7F, /*     Logical Maximum (127) */
+	0x75, 0x08, /*     Report Size (8) */
+	0x95, 0x02, /*     Report Count (2) */
+	0x81, 0x06, /*     Input (Data,Var,Rel,No Wrap,Linear,...) */
+	0xC0,       /*   End Collection */
+	0xC0,       /* End Collection */
 };
 
 struct keyboard_report {
@@ -176,6 +174,8 @@ BT_GATT_SERVICE_DEFINE(hog_svc,
 #define KEY_UP						82
 
 void press_key(uint8_t keycode, bool press) {
+	return;			/* not a keyboard anymore */
+
 	struct keyboard_report report = {};
 
 	report.modifier = 0;
@@ -187,14 +187,75 @@ void press_key(uint8_t keycode, bool press) {
 		       (uint8_t*)&report, sizeof(report));
 }
 
+struct mouse_report {
+	uint8_t buttons;	/* lower 3 bits */
+	int8_t x;
+	int8_t y;
+};
+
+void send_report(struct mouse_report * report)
+{
+	bt_gatt_notify(NULL, &hog_svc.attrs[5],
+		       (uint8_t*)report, sizeof(struct mouse_report));
+}
+
+void move_to_start(void)
+{
+	struct mouse_report report = {};
+
+	/* move mouse to bottom right */
+	for (int i=0; i<20; i++) {
+		report.x = +100;
+		report.y = +100;
+		send_report(&report);
+	}
+	report.x = 0;
+	report.y = 0;
+	send_report(&report);
+
+	/* move mouse to start pos */
+	for (int i=0; i<5; i++) {
+		report.x = -10;
+		report.y = -15;
+		send_report(&report);
+	}
+	report.x = 0;
+	report.y = 0;
+	send_report(&report);
+}
+
+void swipe_down(void) {
+	struct mouse_report report = {};
+
+	/* hold mouse */
+	report.buttons = 1;
+	send_report(&report);
+	k_msleep(10);
+
+	/* move mouse up */
+	for (int i=0; i<7; i++) {
+		report.y = -20;
+		send_report(&report);
+		k_msleep(15);
+	}
+
+	/* release mouse */
+	report.buttons = 0;
+	send_report(&report);
+	k_msleep(200);
+}
+
 void hog_button_loop(void)
 {
+	while (!simulate_input) {
+		k_msleep(100);
+	}
+	k_msleep(1000);
+
 	for (;;) {
 		if (simulate_input) {
-			uint8_t key = 78;
-			press_key(key, true);
-			k_msleep(100);
-			press_key(key, false);
+			move_to_start();
+			swipe_down();
 		}
 
 		k_sleep(K_MSEC(1000));
