02a06ad0b5dcc97b99672ce08fe00de02946257c
Jonathan Rico <jonathan.rico@nordicsemi.no>
2023-09-21 15:27:29 +0200

    make it work on bsim
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -10,10 +10,15 @@
 #include <nrf.h>
 #include <hal/nrf_radio.h>
 
+#include <zephyr/logging/log.h>
+
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
 static uint8_t packet[100];
 
 /* 0x01 -> random 0xc0 -> static */
-static uint8_t adva[6] = {0x13, 0x37, 0x42, 0x69, 0xc0, 0x01};
+static uint8_t adva[6] = {0x01, 0xc0, 0x13, 0x37, 0x42, 0x69};
 
 /* b'\x10\tBluetooth-Shell' */
 static uint8_t advdata[] = {0x10, 0x09, 0x42, 0x6c, 0x75, 0x65, 0x74, 0x6f, 0x6f, 0x74, 0x68, 0x2d, 0x53, 0x68, 0x65, 0x6c, 0x6c};
@@ -25,7 +30,7 @@ void* make_packet(void)
 	uint8_t chsel = 0;	   /* 1 = support chsel algo #2 */
 	uint8_t txadd = 1;	   /* public = 0 random = 1 */
 	uint8_t rxadd = 0;
-	uint8_t length = 0;	/* length of ADV payload */
+	uint8_t length = sizeof(adva) + sizeof(advdata); /* length of ADV payload */
 
 	uint8_t h0 =
 		pdu_type |
@@ -39,25 +44,32 @@ void* make_packet(void)
 	(void)advdata;
 
 	uint32_t pdu_length = sizeof(h0) + sizeof(h1) + sizeof(adva) + sizeof(advdata);
-	printk("length: %d\n", pdu_length);
+	printk("length: %d h0: %x h1: %x\n", pdu_length, h0, h1);
 
 	/* Representation in RAM has only: S0, LENGTH, S1, PAYLOAD */
 
 	/* S0: 0 bytes */
-	packet[0] = pdu_length;	/* LENGTH: 8 bits */
+	uint8_t i = 0;
+	packet[i + 0] = 0x42;
+
+	i++;
+	packet[i + 0] = pdu_length;	/* LENGTH: 8 bits */
 	/* S1: 0 bits */
 
 	/* PAYLOAD starts here */
-	packet[1] = h0;
-	packet[2] = h1;
+	packet[i + 1] = h0;
+	packet[i + 2] = h1;
 
 	/* LE PDU starts here */
-	memcpy(&packet[3], adva, sizeof(adva));
+	memcpy(&packet[i + 3], adva, sizeof(adva));
 
 	/* ADV_IND PDU starts here */
-	memcpy(&packet[3 + sizeof(adva)], advdata, sizeof(advdata));
+	memcpy(&packet[i + 3 + sizeof(adva)], advdata, sizeof(advdata));
 
-	return &packet[0];
+	LOG_HEXDUMP_ERR(packet, pdu_length, "PAYLOAD");
+
+	/* idk what S0 and LENGTH are used for, don't actually pass them to the radio */
+	return &packet[i+1];
 }
 
 int main(void)
@@ -65,59 +77,55 @@ int main(void)
 	printk("init\n");
 	NRF_POWER->TASKS_CONSTLAT = 1;
 	NRF_CLOCK->TASKS_HFCLKSTART = 1;
-	while (!NRF_CLOCK->EVENTS_HFCLKSTARTED) {k_msleep(1);};
+	/* while (!NRF_CLOCK->EVENTS_HFCLKSTARTED) {k_msleep(1);}; */
 
-	NRF_RADIO->POWER = 0;
+	nrf_radio_power_set(NRF_RADIO, 0);
 	k_msleep(200);
-	NRF_RADIO->POWER = 1;
+	nrf_radio_power_set(NRF_RADIO, 1);
 	k_msleep(200);
 
 start:
 	printk("Start loop\n");
-	uint32_t pcnf0 = 0;
-	uint32_t pcnf1 = 0;
 
 	/* Setup automatic START and DISABLE */
 	/* READY->START + END->DISABLE */
-	NRF_RADIO->SHORTS = 3;
+	nrf_radio_shorts_set(NRF_RADIO, 3);
+
+	nrf_radio_txpower_set(NRF_RADIO, NRF_RADIO_TXPOWER_0DBM);
 
 	/* Ble_1Mbit */
-	NRF_RADIO->MODE = 3;
+	nrf_radio_mode_set(NRF_RADIO, 3);
 
 	/* Set Access Address */
 	uint32_t ble_aa = 0x8E89BED6;
-	NRF_RADIO->BASE0 = ble_aa << 8;
-	NRF_RADIO->PREFIX0 = (ble_aa >> 24) & 0xFF;
-	NRF_RADIO->TXADDRESS = 0; /* logical address 0 */
+	nrf_radio_base0_set(NRF_RADIO, ble_aa << 8);
+	nrf_radio_prefix0_set(NRF_RADIO, (ble_aa >> 24) & 0xFF);
+	/* NRF_RADIO->TXADDRESS = 0; /\* logical address 0 *\/ */
 
-	/* Set S0, S1, LENGTH to 0-length */
-	pcnf0 |=
-		8UL | 		/* LENGTH = 8 bits */
-		0 << 8 | 	/* S0 = 0 bytes */
-		0 << 16;	/* S1 = 0 bits */
-
-	/* Preamble config */
-	pcnf0 |= 0 << 24;	/* 1 octet */
-
-	pcnf1 |=
-		0xFF |		/* maxlen */
-		0 << 8 |	/* static len (extra data) */
-		3UL << 16;	/* address is 4 bytes: 3 base addr + 1 prefix */
+	static nrf_radio_packet_conf_t packet_conf = {.lflen = 8UL,
+		.s0len = 1UL,
+		.s1len = 0UL,
+		.s1incl = 0UL,
+		.maxlen = 0xFF,
+		.statlen = 0UL,
+		.balen = 3UL,
+		.big_endian = 0,
+		.whiteen = 1,
+		.plen = NRF_RADIO_PREAMBLE_LENGTH_8BIT};
+	nrf_radio_packet_configure(NRF_RADIO, &packet_conf);
 
 	/* Frequency */
-	NRF_RADIO->FREQUENCY = 26; /* (page 2682) ch38: 2426 MHz */
+	nrf_radio_frequency_set(NRF_RADIO, 2426); /* (page 2682) ch38: 2426 MHz */
 
 	/* CRC:
 	 * 24bit: (x24) + x10 + x9 + x6 + x4 + x3 + x1 + x0
 	 * init: 0x555555
 	 */
-	NRF_RADIO->CRCCNF =
-		3 |	/* length = 24bit */
-		BIT(8); /* skip access addr for calculation */
-	NRF_RADIO->CRCPOLY = BIT(10) | BIT(9) | BIT(6) | BIT(4) | BIT(3) | BIT(1) | BIT(0);
-	NRF_RADIO->CRCINIT = 0x555555;
+	uint32_t crcpoly = BIT(10) | BIT(9) | BIT(6) | BIT(4) | BIT(3) | BIT(1) | BIT(0);
+	nrf_radio_crc_configure(NRF_RADIO, 3, NRF_RADIO_CRC_ADDR_SKIP, crcpoly);
+	nrf_radio_crcinit_set(NRF_RADIO, 0x555555);
 
-	/* Enable whitening:
+	/* Whitening:
 	 * 7bit: x7 + x4 + 1
 	 * init:
 	 * 0: 1
@@ -127,34 +135,32 @@ start:
 	 * register: b6 = 1
 	 * b0 = SR[6]
 	 * b1 = SR[5]
+	 * etc..
 	 */
-	pcnf1 |= BIT(25);	/* WHITEEN */
-	NRF_RADIO->DATAWHITEIV = 38; /* channel 38 */
+	nrf_radio_datawhiteiv_set(NRF_RADIO, 38); /* channel 38 */
 
 	/* Configure the PAYLOAD */
-	NRF_RADIO->PACKETPTR = (uint32_t)make_packet();
+	nrf_radio_packetptr_set(NRF_RADIO, make_packet());
 
 	/* Write packet configuration */
-	NRF_RADIO->PCNF0 = pcnf0;
-	NRF_RADIO->PCNF1 = pcnf1;
 
 	/* Ramp up radio for TX and start TXing right after */
 	printk("Trigger TXEN\n");
 
-	NRF_RADIO->EVENTS_READY = 0;
-	NRF_RADIO->EVENTS_PAYLOAD = 0;
-	NRF_RADIO->EVENTS_DISABLED = 0;
+	nrf_radio_event_clear(NRF_RADIO, NRF_RADIO_EVENT_READY);
+	nrf_radio_event_clear(NRF_RADIO, NRF_RADIO_EVENT_PAYLOAD);
+	nrf_radio_event_clear(NRF_RADIO, NRF_RADIO_EVENT_DISABLED);
 
 	nrf_radio_task_trigger(NRF_RADIO, NRF_RADIO_TASK_TXEN);
-	NRF_RADIO->TASKS_TXEN = 1;
-	while (!NRF_RADIO->EVENTS_READY) {k_msleep(1);};
+	/* NRF_RADIO->TASKS_TXEN = 1; */
+	while (!nrf_radio_event_check(NRF_RADIO, NRF_RADIO_EVENT_READY)) {k_msleep(1);};
 
 	/* Wait until payload has been clocked out */
-	while (!NRF_RADIO->EVENTS_PAYLOAD) {k_msleep(1);};
+	while (!nrf_radio_event_check(NRF_RADIO, NRF_RADIO_EVENT_PAYLOAD)) {k_msleep(1);};
 	printk("got PAYLOAD\n");
 
 	/* Wait until RADIO is off */
-	while (!NRF_RADIO->EVENTS_DISABLED) {k_msleep(1);};
+	while (!nrf_radio_event_check(NRF_RADIO, NRF_RADIO_EVENT_DISABLED)) {k_msleep(1);};
 	printk("got DISABLED\n");
 
 	printk("radio TX ok\n\n");
