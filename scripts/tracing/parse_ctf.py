a33f9ff7ffe080428a6a4dbf13bb73c9c81b3d96
Jonathan Rico <jonathan.rico@nordicsemi.no>
2023-10-05 13:01:34 +0200

    got ISRs working
    seems like nesting doesn't work
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- scripts/tracing/parse_ctf.py
+++ scripts/tracing/parse_ctf.py
@@ -61,6 +61,10 @@ def add_thread(tid, name, active):
 
 def spit_json(path, trace_events):
     trace_events.append(add_metadata("thread_name", 0, 0, {'name': 'general'}))
+    trace_events.append(add_metadata("thread_name", 0, 1, {'name': 'ISR context'}))
+    trace_events.append(add_metadata("thread_name", 0, 2, {'name': 'Mutex'}))
+    trace_events.append(add_metadata("thread_name", 0, 3, {'name': 'Semaphore'}))
+    trace_events.append(add_metadata("thread_name", 0, 4, {'name': 'Timer'}))
     for k in g_thread_names.keys():
         trace_events.append(add_metadata("thread_name", 0, k, {'name': g_thread_names[k]['name']}))
 
@@ -73,6 +77,7 @@ def spit_json(path, trace_events):
         f.write(content)
 
 g_events = []
+g_isr_active = False
 
 def format_json(name, ts, ph, tid=0):
     # Chrome trace format
@@ -91,10 +96,11 @@ def format_json(name, ts, ph, tid=0):
 
     if ph == 'X':
         # fake duration for now
-        evt['dur'] = 100
+        evt['dur'] = 10
 
     return evt
 
+prev_ts = 0
 def main():
     args = parse_args()
 
@@ -108,6 +114,13 @@ def main():
         ph = 'X'
         name = event.name
         tid = 0
+        global g_isr_active
+        global prev_ts
+
+        if prev_ts == ns_from_origin:
+            ns_from_origin += 1
+
+        prev_ts = ns_from_origin
 
         if 'thread' in event.name:
             name = 'thread_active'
@@ -125,10 +138,55 @@ def main():
             # Means that this event tries to mark the thread as active
             already = add_thread(tid, event.payload_field['name'], ph == 'B')
 
-            if already:
+            if already and ph == 'B':
                 # Means the thread is already switched in/out,
                 # adding another event will confuse the UI
+                # It probably means that we are coming from an ISR
+                g_events.append(format_json(event.name, ns_from_origin - 2, 'X', 0))
+                if g_isr_active:
+                    g_isr_active = False
+                    g_events.append(format_json('isr_active', ns_from_origin - 1, 'E', 1))
+
                 return
+
+        elif 'idle' in name:
+            # Means no thread is switched in.
+            # Also a valid way of exiting the ISR
+            if g_isr_active:
+                g_isr_active = False
+                g_events.append(format_json('isr_active', ns_from_origin - 1, 'E', 1))
+
+        elif 'isr' in name:
+            if 'isr_enter' in name:
+                ph = 'B'
+                g_isr_active = True
+            elif 'isr_exit' in name:
+                ph = 'E'
+                g_isr_active = False
+            else:
+                raise(Exception)
+
+            name = 'isr_active'
+            tid = 1
+
+            # debug
+            g_events.append(format_json(event.name, ns_from_origin - 1, 'X', 0))
+
+            if ph == 'B' and g_isr_active:
+                # exit ISR
+                g_events.append(format_json(name, ns_from_origin - 1, 'E', tid))
+
+        elif 'mutex' in name:
+            tid = 2
+
+        elif 'semaphore' in name:
+            tid = 3
+
+        elif 'timer' in name:
+            tid = 4
+
+        # else:
+        #     import pdb;pdb.set_trace()
 
         g_events.append(format_json(name, ns_from_origin, ph, tid))
 
