7d97f7e93c83fe060da6fe218740b68c3c40acfc
Jonathan Rico <jonathan.rico@nordicsemi.no>
2023-11-13 10:33:51 +0100

    tests: trace two bsim tests
    No event left behind (native backend doesn't drop).
    
    The issue with tracing on native_posix/bsim is that since processing
    happens in zero-time the trace looks empty with a lot of events bunched
    up together.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- tests/bsim/bluetooth/host/att/eatt_notif/test_scripts/eatt_notif.sh
+++ tests/bsim/bluetooth/host/att/eatt_notif/test_scripts/eatt_notif.sh
@@ -10,13 +10,20 @@ simulation_id="eatt_notif"
 verbosity_level=2
 EXECUTE_TIMEOUT=120
 
+trace_filepath="$(pwd)/trace_output/ctf_trace"
+mkdir -p $(pwd)/trace_output
+
+bsim_exe=bs_nrf52_bsim_tests_bsim_bluetooth_host_att_eatt_notif_prj_conf
+west build -b nrf52_bsim && \
+    cp build/zephyr/zephyr.exe ${BSIM_OUT_PATH}/bin/${bsim_exe} || false
+
 cd ${BSIM_OUT_PATH}/bin
 
 Execute ./bs_${BOARD}_tests_bsim_bluetooth_host_att_eatt_notif_prj_conf \
-  -v=${verbosity_level} -s=${simulation_id} -d=0 -testid=client
+  -v=${verbosity_level} -s=${simulation_id} -d=0 -testid=client -trace-file=${trace_filepath}_0
 
 Execute ./bs_${BOARD}_tests_bsim_bluetooth_host_att_eatt_notif_prj_conf \
-  -v=${verbosity_level} -s=${simulation_id} -d=1 -testid=server
+  -v=${verbosity_level} -s=${simulation_id} -d=1 -testid=server -trace-file=${trace_filepath}_1
 
 Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
   -D=2 -sim_length=60e6 $@
